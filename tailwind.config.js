d0969026fd072fd7cb6c3c40b8c5fc97bb7ae20a
Ahmed <ahmed@nagy.Nagy>
2025-09-27 14:38:28 +0300

    start in the jsx structute

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.jsx",
